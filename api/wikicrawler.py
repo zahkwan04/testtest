b42e35bfc20bd5327d1a96fac138d1ebb63e59ff
Zahier Arba'ai <36480076+zayozayo@users.noreply.github.com>
2020-02-27 23:51:08 +0800

    Add files via upload

---
+++ api/wikicrawler.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import wikipedia
+import sys
+import requests
+
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+
+def extract_links(url):
+
+page = wikipedia.page(url)
+links = []
+
+for link in links:
+	print link["page"]
+for link in page.links:
+	links.append({"text":link["page"],
+			"href": link["page"]
+			})
+	return links
+	
+
+
+if __name__ == "__main__":
+
+	if len(sys.argv) != 2:
+
+	print("\nUsage:\n\t{} <URL>\n".format(sys.argv[0]))
+
+	sys.exit(1)
+
+for link in extract_links(sys.argv[-1]):
+
+	print("[{}]({})".format(link["text"], link["href"]))
+
